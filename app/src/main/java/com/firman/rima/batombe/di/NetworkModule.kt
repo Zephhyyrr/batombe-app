6abe37f329cba210526f09768e116aef7086b7d9
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2025-11-20 23:52:17 +0700

    production

--- app/src/main/java/com/firman/rima/batombe/di/NetworkModule.kt
+++ app/src/main/java/com/firman/rima/batombe/di/NetworkModule.kt
@@ -1,8 +1,5 @@
 package com.firman.rima.batombe.di
 
-import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerInterceptor
-import com.firman.rima.batombe.BuildConfig
 import com.firman.rima.batombe.data.remote.service.AnalyzeService
 import com.firman.rima.batombe.data.remote.service.ArticleService
 import com.firman.rima.batombe.data.remote.service.ExampleVideoService
@@ -20,7 +17,6 @@ import com.firman.rima.batombe.utils.ApiConstant
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -35,22 +31,12 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideChuckerInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
-        return ChuckerInterceptor.Builder(context).build()
-    }
-
-    @Provides
-    @Singleton
     fun provideOkHttpClient(
-        chuckerInterceptor: ChuckerInterceptor
     ): OkHttpClient {
         val builder = OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.MINUTES)
             .readTimeout(30, TimeUnit.MINUTES)
 
-        if (BuildConfig.DEBUG) {
-            builder.addInterceptor(chuckerInterceptor)
-        }
         return builder.build()
     }
 
