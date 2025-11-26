bc1769f3f1514a997799d6af32c9a028f3597370
Firman Ardiansyah <ardiansyahfirman2004@gmail.com>
2025-11-26 13:18:57 +0700

    update: ci with circle ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -152,6 +152,4 @@ dependencies {
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
-    implementation(libs.firebase.appdistribution)
-
 }
